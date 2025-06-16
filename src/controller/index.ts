dccb7823d1531ea6d144642d747e5459fd8aafba
hseun <hseun0732@dms.hs.kr>
2025-06-16 14:47:47 +0900

    feature :: accountBankNumber

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,5 +1,5 @@
 import express, { Response } from 'express';
-import { getApiLimit } from '../middleware/limit';
+import { apiLimit, getApiLimit } from '../middleware/limit';
 import { verifyJWT } from '../middleware/jwt';
 import { AuthenticatedRequest } from '../types';
 import service from '../service';
@@ -11,6 +11,9 @@ app.get('/', getApiLimit, verifyJWT, (req: AuthenticatedRequest, res: Response) 
 });
 app.get('/money', getApiLimit, verifyJWT, (req: AuthenticatedRequest, res: Response) => {
   service.getBankMoney(req, res);
+});
+app.post('/', apiLimit, verifyJWT, (req: AuthenticatedRequest, res: Response) => {
+  service.accountBankNumber(req, res);
 });
 
 export default app;
---
+++ src/service/accountBankNumber.ts
@@ -0,0 +1,40 @@
+import { prisma } from '../config/prisma';
+import { AuthenticatedRequest, BasicResponse } from '../types';
+import { Response } from 'express';
+
+export const accountBankNumber = async (req: AuthenticatedRequest, res: Response<BasicResponse>) => {
+  try {
+    const userId = req.userId;
+    if (!userId) {
+      return res.status(401).json({
+        message: '토큰 검증 실패'
+      });
+    }
+
+    const { bankName, bankNumber, bankNumberMasked, apiTranId, alias } = req.body;
+    if (!bankName || !bankNumber || !bankNumberMasked || !apiTranId || !alias) {
+      return res.status(400).json({
+        message: '올바르지 않은 입력값'
+      });
+    }
+
+    await prisma.bank.create({
+      data: {
+        userId: userId,
+        bankName: bankName,
+        bankNumber: bankNumber,
+        bankNumberMasked: bankNumberMasked,
+        apiTranId: apiTranId
+      }
+    });
+
+    return res.status(201).json({
+      message: '계좌 등록 성공'
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      message: '서버 오류 발생'
+    });
+  }
+};
